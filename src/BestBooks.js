2f140454f7cfe69be97a8a10656fbec7265e0461
Ali Al-hjouj <alisalamehhjouj@gmail.com>
2021-08-17 16:31:56 +0300

    add

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,9 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
 import axios from 'axios';
-import { Card, ListGroup } from 'react-bootstrap';
+import { Card, ListGroup,Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { withAuth0 } from "@auth0/auth0-react";
+import BookFormMOdel from './components/BookFormMOdel';
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class MyFavoriteBooks extends React.Component {
 
     this.state = {
       bookData: [],
+      show:false
     }
   }
 
@@ -25,29 +27,74 @@ class MyFavoriteBooks extends React.Component {
       })
     })
   }
+  addBook=()=>{
+    this.setState({
+      show:true
+    })
+  }
+  closeHandler=()=>{
+    this.setState({
+      show:false
+    })
+  }
+  bookCreat=(e)=>{
+    e.preventDefault();
+    const data={
+      email:this.props.auth0.user.email,
+      bookTitle:e.target.bookTitle.value,
+      bookDescription:e.target.bookDescription.value,
+      bookStatus:e.target.bookStatus.value,
+    }
+    console.log('0')
+    axios.post(`http://localhost:3001/book-creation`,data).then(item =>{
+      console.log('1')
+
+      this.setState({
+        bookData:item.data
+      })
+    })
+  }
+  deleteBook=(index)=>{
+    console.log(index)
+    const data={
+      email:this.props.auth0.user.email,
+    }
+    
+    axios.delete(`http://localhost:3001/book-delete/${index}`,{params:data}).then(item1 =>{
+      console.log('1')
+      this.setState({
+        bookData:item1.data
+      })
+    })
+  }
   render() {
     return (
       <>
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
-      <main className="card">
-        {this.state.bookData.length > 0&&
-        this.state.bookData.map(item => {
-         return (<Card  style={{ width: '300px'}}>
-           <ListGroup  >
-             <ListGroup.Item variant="dark">{item.title}</ListGroup.Item>
-             <ListGroup.Item>{item.description}</ListGroup.Item>
-             <ListGroup.Item variant="warning">by:{item.status}</ListGroup.Item>
-           </ListGroup>
-         </Card>)
-       })
-     }
-     </main>
-</>
+        <Jumbotron>
+          <h1>My Favorite Books</h1>
+          <p>
+            This is a collection of my favorite books
+          </p>
+          <Button onClick={this.addBook} variant="primary" size="lg">
+            Block level button
+          </Button>
+        </Jumbotron>
+        <BookFormMOdel bookCreat={this.bookCreat} close={this.closeHandler} show={this.state.show}/>
+        <main className="card">
+          {this.state.bookData.length > 0 &&
+            this.state.bookData.map((item,index) => {
+              return (<Card style={{ width: '300px' }}>
+                <ListGroup key={index} >
+                  <ListGroup.Item variant="dark">{item.title}</ListGroup.Item>
+                  <ListGroup.Item>{item.description}</ListGroup.Item>
+                  <ListGroup.Item variant="warning">by:{item.status}</ListGroup.Item>
+                  <button onClick={()=>this.deleteBook(index)}>Delete</button>
+                </ListGroup>
+              </Card>)
+            })
+          }
+        </main>
+      </>
     )
   }
 }
